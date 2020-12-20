e17d86ffca1967afc1a11cad6bd91dbe2ff6bde4
twomiao <995200452@qq.com>
2020-12-21 00:03:42 +0800

    xxx

---
+++ easypay/Test/MakeOrder.php
@@ -0,0 +1,29 @@
+<?php
+try {
+    $config = array(
+        "fxnotifyurl" => "http://www.xxxx.com/index.php/index/test",
+        'fxbackurl' => 'http://www.xxx.com/index.php',
+        'fxpay' => 'wxhf', // wxsm
+        'merchant_key' => '',
+        'merchant_id' => 'xxx',
+        'gateway_url' => 'http://xxx.xxx.com/Pay',
+        'is_debug' => true,
+        'log_file' => 'WePay.log'
+    );
+
+    $wePay = new \Easypay\WePay\WePay($config);
+    $order = array(
+        "order_no" => date('YmdHis') . mt_rand(1000, 9999),
+        "product_name" => 'payTest',
+        "order_price" => 30,
+        "client_ip" => '118.112.56.13',
+        'extra' => 'payTest'
+    );
+    // 保存订单数数据
+    $wePay->setOrderItem($order);
+
+    // 跳转付款页面
+    $wePay->goWePay();
+} catch (\Easypay\WePay\InvalidResponseException $e) {
+    echo $e->getMessage();
+}
